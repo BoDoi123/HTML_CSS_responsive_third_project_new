c2fdb51b62d7e2ceaa2927676f977bdca606944d
Chu Long <chulong_t65@hus.edu.vn>
2025-01-16 01:14:02 +0700

    JavaScript _ DOM JS - Parent, Children & Siblings

--- js/main.js
+++ js/main.js
@@ -5,6 +5,29 @@ for (let btn of buttons) {
         btn.style.backgroundColor = "blue";
         btn.innerHTML = "Click me";
         btn.style.fontSize = "2em";
-        btn.style.color = "yellow"; 
+        btn.style.color = "yellow";
     }
 }
+
+// Truy cập Parent, Child and Siblings
+// Ví dụ:
+let elm = document.querySelector(".hero__content");
+
+// 1. Truy cap vao phan tu cha (Phan tu cha la duy nhat)
+console.log(elm.parentElement);
+
+// Doi mau nen cua the cha cua elm
+// elm.parentElement.style.backgroundColor = "red";
+
+// 2. Truy cap vao phan tu con (Phan tu con co the co nhieu)
+console.log(elm.children); // Tra ve 1 HTMLCollection(3)
+
+console.log(elm.children[0]);
+console.log(elm.children[1]);
+console.log(elm.children[2]);
+
+// 3. nextElementSiblings: chon phan tu anh em ke tiep
+console.log(elm.children[1].nextElementSibling);
+
+// 4. previousElementSiblings: chon phan tu anh em truoc do
+console.log(elm.children[1].previousElementSibling);
